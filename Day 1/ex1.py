3ae3a2cc74f299cc18adabbb3bcaf8e7d44a9e35
DawodGAMIETTE <david.gamiette@gmail.com>
2024-12-02 23:07:02 +0100

    add Day1 Ex1

---
+++ Day 1/ex1.py
@@ -0,0 +1,24 @@
+print("Day 1 - Exercise 1")
+
+left_values = []
+right_values = []
+
+with open('input-ex1.txt', 'r') as input:
+    for line in input:
+        left, right = map(int, line.split())
+        left_values.append(left)
+        right_values.append(right)
+
+left_values.sort()
+right_values.sort()
+
+distance = []
+total_distance = 0
+
+for i in range(len(left_values)):
+    distance.append(abs(left_values[i] - right_values[i]))
+
+for i in range(len(distance)):
+    total_distance = total_distance + distance[i]
+
+print("Total distance:", total_distance)
